1dee085b879804f1d1749802f314986634301127
Gaders <962224726@qq.com>
2018-11-27 10:31:32 +0800

    bate 4.0

--- js1.js
+++ js1.js
@@ -1,6 +1,7 @@
 var c = 0
-
-
+var b =0
+var d = 0
+ var flag = 0
 window.onload=function() {
 
 
@@ -14,15 +15,15 @@ var fasong=document.querySelector(".fasong")
       }
            
           
-     else if(c < 5){
-        c = c+1
+     else if(d < 5){
+        d = d+1
         var neirong = document.getElementsByClassName("neirong")[0]
         var a =neirong.value
         var b =document.getElementsByClassName("nicheng")[0].value
-        var p=document.querySelector("#first-"+c+"")
-        p.insertAdjacentHTML("afterbegin","<p class='the"+c+"'>"+"<br>"+"用户名:"+b+"</br>"+"留言:"+a+"</p>")
-        document.querySelector(".dianzan"+c+"").style.cssText="display:inline"
-        document.querySelector(".pinglun"+c+"").style.cssText="display:inline"
+        var p=document.querySelector("#first-"+d+"")
+        p.insertAdjacentHTML("afterbegin","<p class='the"+d+"'>"+"<br>"+"用户名:"+b+"</br>"+"留言:"+a+"</p>")
+        document.querySelector(".dianzan"+d+"").style.cssText="display:inline"
+        document.querySelector(".pinglun"+d+"").style.cssText="display:inline"
         document.getElementsByClassName("neirong")[0].value= ""
         document.getElementsByClassName("nicheng")[0].value= ""
         
@@ -35,16 +36,18 @@ var fasong=document.querySelector(".fasong")
 
 var neirong = document.querySelector(".neirong")
 neirong.addEventListener("click",function(){
-  if(document.getElementsByClassName("neirong")[0].value == "请在此输入内容--By⑨"){
-    document.getElementsByClassName("neirong")[0].value = ""
-    document.getElementsByClassName("neirong")[0].style.cssText="color:black"
+    c = c +1
+  if(c == "1"){
+     document.getElementsByClassName("neirong")[0].value = ""
+     document.getElementsByClassName("neirong")[0].style.cssText="color:black"
     }
     
 
   })
-var neirong = document.querySelector(".nicheng")
-neirong.addEventListener("click",function(){
-  if(document.getElementsByClassName("nicheng")[0].value == "请在此输入昵称--By⑨"){
+var nicheng = document.querySelector(".nicheng")
+nicheng.addEventListener("click",function(){
+  b = b +1
+  if(b=="1"){
     document.getElementsByClassName("nicheng")[0].value = ""
     document.getElementsByClassName("nicheng")[0].style.cssText="color:black"
   }
@@ -55,33 +58,67 @@ neirong.addEventListener("click",function(){
 
 var dianzan = document.querySelector("#first")
 dianzan.addEventListener("click",function(e){
- if ((e.target.className.replace("dianzan","")== "1" || e.target.className.replace("dianzan","")== "2" ||e.target.className.replace("dianzan","")== "3" ||e.target.className.replace("dianzan","")== "4" ||e.target.className.replace("dianzan","")== "5" )&& document.querySelector(".dianzan1").value == "点赞"){
+    
+    flag= flag+1
+ if ((flag % 2 == 1)&&(e.target.className.replace("dianzan","")== "1" || e.target.className.replace("dianzan","")== "2" ||e.target.className.replace("dianzan","")== "3" ||e.target.className.replace("dianzan","")== "4" ||e.target.className.replace("dianzan","")== "5" )&& (document.querySelector(".dianzan1").value == "点赞"||document.querySelector(".dianzan2").value == "点赞"||document.querySelector(".dianzan3").value == "点赞"||document.querySelector(".dianzan4").value == "点赞"||document.querySelector(".dianzan5").value == "点赞")){
     var str = e.target.className
-    var a = str.replace("dianzan","")
-    document.getElementsByClassName("the"+a)[0].style.cssText="color:red"
-    document.querySelector(".dianzan"+a).value="取消赞"
+    var z = str.replace("dianzan","")
+    document.getElementsByClassName("the"+z)[0].style.cssText="color:red"
+    document.querySelector(".dianzan"+z).value="取消赞"
+    
     }
-  else if(document.querySelector(".dianzan1").value == "取消赞"||document.querySelector(".dianzan2").value == "取消赞"||document.querySelector(".dianzan3").value == "取消赞"||document.querySelector(".dianzan4").value == "取消赞"||document.querySelector(".dianzan5").value == "取消赞"){
+ else if (flag % 2 == 0  &&(e.target.className.replace("dianzan","")== "1" || e.target.className.replace("dianzan","")== "2" ||e.target.className.replace("dianzan","")== "3" ||e.target.className.replace("dianzan","")== "4" ||e.target.className.replace("dianzan","")== "5" ))
+    dianzan.addEventListener("click",function(e){
     var str = e.target.className
     var a = str.replace("dianzan","")
     document.getElementsByClassName("the"+a)[0].style.cssText="color:black"
-    document.querySelector(".dianzan"+a).value="不给赞了"
-  }
- 
+    document.querySelector(".dianzan"+a).value="点赞"
+    
+},true)
+
 })
-  
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 var pinglun = document.querySelector("#first")
   pinglun.addEventListener("click",function(e){
     if(e.target.className.replace("pinglun","")== "1" || e.target.className.replace("pinglun","")== "2" ||e.target.className.replace("pinglun","")== "3" ||e.target.className.replace("pinglun","")== "4" ||e.target.className.replace("pinglun","")== "5" ){
-      var str = e.target.className
-      var a = str.replace("pinglun","")
-      pinglunNicheng = prompt("请输入你的昵称")
-      pinglunNerrong = prompt("请输入想评论的内容")
-      var p=document.querySelector(".the"+a+"")
-      p.insertAdjacentHTML("afterend","<div id='kong'></div><p class='theT"+a+"'>"+"<br>"+"评论者:"+ pinglunNicheng+"</br>"+"评论:"+pinglunNerrong+"</p>")
+     
+    var pinglunNicheng = prompt("请输入你的昵称")
+    var pinglunNerrong = prompt("请输入想评论的内容")
+    if(pinglunNicheng == null ||  pinglunNerrong == null){alert("评论失败了嘤嘤嘤")}
+    else{
+      
+    var str = e.target.className
+    var k = str.replace("pinglun","")
+    var p=document.querySelector(".the"+k+"")
+    p.insertAdjacentHTML("afterend","<div id='kong'></div><p class='theT"+k+"'>"+"<br>"+"评论者:"+ pinglunNicheng+"</br>"+"评论:"+pinglunNerrong+"</p>")
+ 
+  }
+    
     }
-  })
+
+
+
+
+    
+  })  
+  
 
 
 
